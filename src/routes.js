03c2efccb797968166b36aaaef0a06c074bc5eae
Matheus <szmatheussouza@gmail.com>
2024-10-13 14:55:09 -0300

    estruturacao-project

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+
+function RoutesApp(){
+    return(
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/" element={ <Home/> }/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RoutesApp;
